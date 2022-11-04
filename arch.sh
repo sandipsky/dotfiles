6798c315183ba81d161dc7249d717c6d2ac9c34f
sandipsky <sandipshakya75@gmail.com>
2022-11-04 09:16:42 +0545

    xfceglobalmenu

--- arch.sh
+++ arch.sh
@@ -20,18 +20,18 @@ curl -sS https://starship.rs/install.sh | sh
 
 sudo cp wall/* /usr/share/backgrounds
 sudo cp -r themes/* /usr/share/themes
-sudo cp -r icons/* /usr/share/iconss
+sudo cp -r icons/* /usr/share/icons
 
 
 
-sudo su -c "echo '/dev/nvme0n1p4 /media/sandipsky ntfs nls-utf8,umask-0755,uid-1000,gid-1000,rw 0 0' >> /etc/fstab"
+#sudo su -c "echo '/dev/nvme0n1p4 /media/sandipsky ntfs nls-utf8,umask-0755,uid-1000,gid-1000,rw 0 0' >> /etc/fstab"
 
 
 
 sudo cp 90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
 
-sudo rm -rf /usr/share/applications/*
-sudo cp desktopfiles/xfce/* /usr/share/applications
+#sudo rm -rf /usr/share/applications/*
+#sudo cp desktopfiles/xfce/* /usr/share/applications
 
 #AUR
 cd ~
