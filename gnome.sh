aa53ec6facc287e85090481484ce9465526bacea
sandipsky <sandipshakya75@gmail.com>
2023-03-25 20:54:39 +0545

    arch script

--- gnome.sh
+++ gnome.sh
@@ -2,6 +2,8 @@
 
 sudo pacman -S gnome-shell gnome-control-center gnome-calendar gnome-calculator nautilus gnome-system-monitor gnome-tweaks gnome-screenshot gnome-terminal gnome-shell-extensions evince eog gedit file-roller xdg-user-dirs-gtk firefox vlc rhythmbox neofetch cmatrix gdm gnome-keyring ntfs-3g gvfs-mtp ffmpegthumbnailer ttf-fira-sans noto-fonts noto-fonts-emoji --noconfirm --needed
 
+yay -S visual-studio-code-bin plata-theme-bin brave-bin --needed --noconfirm
+
 sudo systemctl enable gdm
 
 exit
--- kde.sh
+++ kde.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S plasma konsole sddm ark dolphin spectacle gwenview kalendar okular kcalc ffmpegthumbs xdg-user-dirs firefox vlc rhythmbox neofetch cmatrix ntfs-3g gvfs-mtp ffmpegthumbnailer noto-fonts noto-fonts-emoji libreoffice-fresh sddm --noconfirm --needed
+sudo pacman -S plasma konsole sddm ark dolphin spectacle nomacs kalendar okular kcalc ffmpegthumbs xdg-user-dirs firefox vlc rhythmbox neofetch cmatrix ntfs-3g gvfs-mtp ffmpegthumbnailer noto-fonts noto-fonts-emoji ttf-fira-sans sddm --noconfirm --needed
+
+yay -S visual-studio-code-bin brave-bin --needed --noconfirm
 
 sudo systemctl enable sddm
 
