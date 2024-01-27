8a54f68ab5b719c7161e7dbc2b54a5da91e87284
sandipsky <sandipshakya75@gmail.com>
2024-01-27 22:29:14 +0545

    changes

--- gnome.sh
+++ gnome.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions gnome-shell gnome-control-center gnome-calculator nautilus python-nautilus gnome-tweaks gnome-terminal gnome-themes-extra evince loupe gnome-text-editor file-roller xdg-user-dirs-gtk firefox vlc rhythmbox neofetch gdm gnome-keyring ntfs-3g gvfs-mtp ffmpegthumbnailer xdg-desktop-portal-gnome ttf-liberation noto-fonts noto-fonts-emoji noto-fonts-extra gnome-system-monitor gnome-color-manager gnome-characters gnome-backgrounds gnome-font-viewer gnome-disk-utility gnome-screenshot gnome-shell-extensions power-profiles-daemon switcheroo-control --noconfirm --needed
+
 curl -sS https://starship.rs/install.sh | sh
 
 ##Workspace Keyboard
@@ -16,12 +19,17 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 56
 ##New file context menu
 touch ~/Templates/NewDocument.txt
 
-sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+sudo -s /dev/null /etc/udev/rules.d/61-gdm.rules
+
+sh aur.sh
+sh ntfs.sh
+sh battery.sh
 
 rm rf /home/sandip/.bashrc
 cp .zshrc /home/sandip
 cp .zprofile /home/sandip
 
-chsh -s /bin/zsh sandip
+sudo systemctl enable gdm switcheroo-control
+sudo chsh -s /bin/zsh
 
 
