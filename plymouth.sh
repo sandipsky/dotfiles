0f5de06fbe90f93f166b9f444978bdb9219fad9c
sandipsky <sandipshakya75@gmail.com>
2024-05-15 20:32:49 +0545

    changes

--- plymouth.sh
+++ plymouth.sh
@@ -3,6 +3,7 @@
 sudo pacman -S plymouth --needed --noconfirm
 sudo plymouth-set-default-theme -R bgrt
 sudo rm /usr/share/plymouth/themes/spinner/watermark.png
+sudo mkinitcpio -P
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash loglevel=3 systemd.show_status=false rd.udev.log_level=3"/' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
