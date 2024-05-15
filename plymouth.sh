31dc1b96dfdb9885b097dd0bd76df0e853625caa
sandipsky <sandipshakya75@gmail.com>
2024-05-15 20:36:54 +0545

    changes

--- plymouth.sh
+++ plymouth.sh
@@ -9,6 +9,6 @@ sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo sed -i "/echo    'Loading Linux linux ...'/d" /boot/grub/grub.cfg
-sudo sed -i "/echo    'Loading initial ramdisk ...'/d" /boot/grub/grub.cfg
+sudo sed -i "/        echo    'Loading Linux linux ...'/d" /boot/grub/grub.cfg
+sudo sed -i "/        echo    'Loading initial ramdisk ...'/d" /boot/grub/grub.cfg
 
