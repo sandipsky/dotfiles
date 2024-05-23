0e29fdde165277048727a7a2e92a0b380a2274b5
sandip-annofi <sandip.shakya@annofi.com>
2024-05-23 16:31:26 +0545

    changes

---
+++ hyprland/hyprland.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -S hyprland hyprpaper xdg-desktop-portal-hyprland waybar grim slurp cliphist hyprlock hypridle --noconfirm --needed
+
+sudo pacman -S nomacs blueman tlp network-manager-applet zsh zsh-syntax-highlighting zsh-autosuggestions rhythmbox atril noto-fonts noto-fonts-emoji noto-fonts-extra rofi-wayland vlc firefox ntfs-3g tumbler ffmpegthumbnailer file-roller alacritty xdg-user-dirs-gtk neofetch mousepad gvfs-mtp galculator wget thunar thunar-archive-plugin noto-fonts noto-fonts-emoji ttf-dejavu otf-font-awesome ttf-dejavu-nerd ttf-fira-sans starship dunst libadwaita brightnessctl --noconfirm --needed
+
+wlogout emote 
