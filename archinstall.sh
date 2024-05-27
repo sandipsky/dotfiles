182151dbddfd0329e42b24ac51b88ef6f9989b00
Sandip Shakya <sandip@archlinux.localdomain>
2024-05-27 21:55:33 +0545

    changes

--- archinstall.sh
+++ archinstall.sh
@@ -3,11 +3,21 @@
 echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
 read EFI
 
-echo "Do you want to FORMAT EFI PARTITION? Yy/Nn (Choose yes for newly created Partition)"
-read ISFORMAT
-
 echo "Please enter Root(/) paritition: (example /dev/sda3)"
 read ROOT  
+
+while true; do
+    echo "Please choose Bootloader"
+    echo "1. GRUB"
+    echo "2. Systemd-boot"
+    read -p "Enter your choice: " BOOT
+
+    if [ "$BOOT" == "1" ] || [ "$BOOT" == "2" ]; then
+        break
+    else
+        echo "Invalid input. Please enter 1 or 2."
+    fi
+done
 
 echo "Please enter your Username"
 read USER 
@@ -21,24 +31,46 @@ read PASSWORD
 # make filesystems
 echo -e "\nCreating Filesystems...\n"
 
-if [[ $ISFORMAT == 'y' || $ISFORMAT == 'Y' ]]
-then 
-    mkfs.vfat -F32 "${EFI}"
-fi 
+existing_fs=$(blkid -s TYPE -o value "$EFI")
+if [[ "$existing_fs" != "vfat" ]]; then
+    mkfs.vfat -F32 "$EFI"
+fi
 
 mkfs.ext4 "${ROOT}"
 
 # mount target
 mount "${ROOT}" /mnt
-mount --mkdir "${EFI}" /mnt/boot/efi
+
+if [ "$BOOT" == "1" ]; then
+    mount --mkdir "${EFI}" /mnt/boot/efi
+elif [ "$BOOT" == "2" ]; then
+    mount --mkdir "${EFI}" /mnt/boot
+fi
 
 echo "--------------------------------------"
 echo "-- INSTALLING Base Arch Linux --"
 echo "--------------------------------------"
-pacstrap /mnt base base-devel linux linux-firmware linux-headers networkmanager network-manager-applet wireless_tools nano intel-ucode bluez bluez-utils git --noconfirm --needed
+pacstrap /mnt base base-devel linux linux-firmware linux-headers networkmanager wireless_tools nano intel-ucode bluez bluez-utils git --noconfirm --needed
 
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+
+if [ "$BOOT" == "2" ]; then
+echo "--------------------------------------"
+echo "-- Bootloader Systemd Installation  --"
+echo "--------------------------------------"
+
+ROOT_UUID=$(blkid -s UUID -o value $ROOT)
+
+bootctl install --path /mnt/boot
+echo "default arch.conf" >> /mnt/boot/loader/loader.conf
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=UUID=$ROOT_UUID rw quiet splash fsck.mode=skip loglevel=3 systemd.show_status=false rd.udev.log_level=3
+EOF
+fi
 
 cat <<REALEND > /mnt/next.sh
 useradd -m $USER
@@ -77,9 +109,11 @@ echo "--------------------------------------"
 echo "-- Bootloader Installation  --"
 echo "--------------------------------------"
 
-pacman -S grub efibootmgr --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux"
-grub-mkconfig -o /boot/grub/grub.cfg
+if [ "$BOOT" == "1" ]; then
+    pacman -S grub efibootmgr --noconfirm --needed
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux"
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
 
 cd /home/sandip
 git clone https://github.com/sandipsky/dotfiles
