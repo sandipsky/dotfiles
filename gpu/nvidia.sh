9e677e9b5321d78b8b8fc1b87745fb9e80edf119
sandipsky <sandipshakya75@gmail.com>
2024-06-05 21:35:31 +0545

    change

--- gpu/nvidia.sh
+++ gpu/nvidia.sh
@@ -1,4 +1,4 @@
-sudo pacman -S nvidia-prime -y
+sudo pacman -S nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime -y
 
 sudo rm -rf /etc/modprobe.d/blacklist-nvidia.conf /lib/udev/rules.d/50-remove-nvidia.rules
 sudo rm -rf /etc/X11/xorg.conf.d/90-nvidia.conf /etc/modprobe.d/nvidia.conf
