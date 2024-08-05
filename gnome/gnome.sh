64c0685d74531cbbcd4fc99c44f82d4ef9475c4a
sandipsky <sandipshakya75@gmail.com>
2024-08-05 11:56:15 +0545

    changes

--- gnome/gnome.sh
+++ gnome/gnome.sh
@@ -11,21 +11,6 @@ sudo pacman -S wget vlc rhythmbox neofetch ntfs-3g qbittorrent switcheroo-contro
 
 sudo systemctl enable gdm switcheroo-control 
 
-sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Software.svg
-sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Nautilus.svg
-sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Terminal.svg
-
-cd icons
-sudo cp * /usr/share/icons/hicolor/scalable/apps/
-
-sudo rm /usr/share/applications/avahi-discover.desktop 
-sudo rm /usr/share/applications/qv4l2.desktop 
-sudo rm /usr/share/applications/qvidcap.desktop 
-sudo rm /usr/share/applications/bvnc.desktop   
-sudo rm /usr/share/applications/bssh.desktop 
-sudo rm /usr/share/applications/lstopo.desktop
-sudo rm /usr/share/applications/nm-connection-editor.desktop
-
 touch ~/Templates/NewDocument.txt
 
 cd ..
@@ -40,15 +25,33 @@ yay -S auto-cpufreq google-chrome visual-studio-code-bin --noconfirm --needed
 sudo auto-cpufreq --install
 
 cd ..
-cd gnome
-cd extensions
-sudo rm -r /usr/share/gnome-shell/extensions/* 
-sudo cp -r * /usr/share/gnome-shell/extensions 
-
-cd ..
 cd keybindings
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom 
 dconf load /org/gnome/desktop/wm/keybindings/ < wm  
+
+sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Software.svg
+sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Nautilus.svg
+sudo rm /usr/share/icons/hicolor/scalable/apps/org.gnome.Terminal.svg
+
+sudo rm /usr/share/applications/avahi-discover.desktop 
+sudo rm /usr/share/applications/qv4l2.desktop 
+sudo rm /usr/share/applications/qvidcap.desktop 
+sudo rm /usr/share/applications/bvnc.desktop   
+sudo rm /usr/share/applications/bssh.desktop 
+sudo rm /usr/share/applications/lstopo.desktop
+sudo rm /usr/share/applications/nm-connection-editor.desktop
+
+cd ..
+cd ..
+cd assets
+cd icons
+sudo cp org.gnome.Software.svg /usr/share/icons/hicolor/scalable/apps/org.gnome.Software.svg
+sudo cp org.gnome.Nautilus.svg /usr/share/icons/hicolor/scalable/apps/org.gnome.Nautilus.svg
+sudo cp org.gnome.Terminal.svg /usr/share/icons/hicolor/scalable/apps/org.gnome.Terminal.svg
+sudo cp ms-excel.svg /usr/share/icons/hicolor/scalable/apps/ms-excel.svg
+sudo cp ms-word.svg /usr/share/icons/hicolor/scalable/apps/ms-word.svg
+sudo cp ms-powerpoint.svg /usr/share/icons/hicolor/scalable/apps/ms-powerpoint.svg
+
 
 sudo reboot
 
