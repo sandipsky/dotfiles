9a6f60a008879ff86818a13cd5ee45f405df5675
sandipsky <sandipshakya75@gmail.com>
2024-08-06 20:56:28 +0545

    changes

--- gnome/gnome.sh
+++ gnome/gnome.sh
@@ -4,7 +4,7 @@
 sudo pacman -S gnome-shell gnome-control-center gnome-calculator gnome-menus colord-gtk nautilus python-nautilus ffmpegthumbnailer gvfs-mtp file-roller xdg-desktop-portal-gnome gnome-tweaks gnome-software malcontent flatpak gnome-terminal gnome-themes-extra gnome-color-manager gnome-backgrounds gnome-disk-utility gnome-screenshot gnome-shell-extensions evince loupe gnome-text-editor xdg-user-dirs-gtk gdm gnome-keyring --noconfirm --needed
 
 #FONTS
-sudo pacman -S ttf-liberation noto-fonts-cjk ttf-fira-sans ttf-jetbrains-mono noto-fonts noto-fonts-emoji noto-fonts-extra --noconfirm --needed
+sudo pacman -S ttf-liberation noto-fonts-cjk ttf-jetbrains-mono noto-fonts noto-fonts-emoji noto-fonts-extra --noconfirm --needed
 
 #BASIC PROGRAMS
 sudo pacman -S wget vlc rhythmbox neofetch ntfs-3g qbittorrent switcheroo-control --noconfirm --needed
