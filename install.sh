fda0b0c23c2027815e85127a97aab1921d783d17
sandipsky <sandipshakya75@gmail.com>
2025-05-12 11:51:14 +0545

    changes

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 USERNAME="sandip"
 
-sudo pacman -S hyprland hyprpaper xdg-desktop-portal-hyprland waybar hyprlock hypridle blueman rhythmbox atril rofi-wayland vlc ntfs-3g tumbler ffmpegthumbnailer file-roller alacritty xdg-user-dirs-gtk neofetch mousepad gvfs-mtp wget thunar obs-studio qbittorrent thunar-archive-plugin noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation noto-fonts-cjk ttf-dejavu ttf-font-awesome ttf-fira-sans starship dunst adwaita-icon-theme brightnessctl ttf-jetbrains-mono gnome-themes-extra wlsunset grim slurp gdm --noconfirm --needed
+sudo pacman -S hyprland hyprpaper xdg-desktop-portal-hyprland waybar hyprlock hypridle blueman rofi-wayland vlc ntfs-3g ffmpegthumbnailer alacritty xdg-user-dirs-gtk neofetch wget obs-studio qbittorrent noto-fonts noto-fonts-emoji noto-fonts-extra ttf-liberation noto-fonts-cjk ttf-dejavu ttf-font-awesome ttf-fira-sans starship dunst adwaita-icon-theme brightnessctl ttf-jetbrains-mono wlsunset grim slurp --noconfirm --needed
+
+#GNOME APPS
+sudo pacman -S gnome-music evince loupe gnome-calendar gnome-calculator file-roller gnome-text-editor gvfs-mtp gnome-themes-extra gdm --noconfirm --needed
+
+#FONTS
 
 cd scripts
 
@@ -15,7 +20,7 @@ sh git.sh
 sh gaming.sh
 sh programs.sh
 
-yay -S nomacs ttf-material-design-icons-desktop-git ttf-meslo-nerd-font-powerlevel10k google-chrome visual-studio-code-bin wlogout --noconfirm --needed
+yay -S ttf-material-design-icons-desktop-git ttf-meslo-nerd-font-powerlevel10k google-chrome gdm-settings visual-studio-code-bin wlogout --noconfirm --needed
 
 cd ..
 
@@ -26,7 +31,6 @@ rm -rf /home/$USERNAME/.config/*
 cp -r alacritty /home/$USERNAME/.config/
 cp -r hypr /home/$USERNAME/.config/
 cp -r rofi /home/$USERNAME/.config/
-cp -r Thunar /home/$USERNAME/.config/
 cp -r waybar /home/$USERNAME/.config/
 cp -r dunst /home/$USERNAME/.config/
 
@@ -47,18 +51,9 @@ sudo rm /usr/share/applications/bssh.desktop
 sudo rm /usr/share/applications/bvnc.desktop
 sudo rm /usr/share/applications/rofi.desktop
 sudo rm /usr/share/applications/rofi-theme-selector.desktop
-sudo rm /usr/share/applications/thunar-bulk-rename.desktop
-sudo rm /usr/share/applications/thunar-settings.desktop
 sudo rm /usr/share/applications/qv4l2.desktop
 sudo rm /usr/share/applications/qvidcap.desktop
-sudo rm /usr/share/applications/mate-color-select.desktop
-sudo rm /usr/share/applications/xfce4-about.desktop
-sudo rm /usr/share/applications/org.xfce.mousepad-settings.desktop
 sudo rm /usr/share/applications/Alacritty.desktop
-sudo rm /usr/share/applications/atril.desktop
-sudo rm /usr/share/applications/org.nomacs.ImageLounge.desktop
-sudo rm /usr/share/applications/org.xfce.mousepad.desktop
-sudo rm /usr/share/applications/thunar.desktop
 sudo rm /usr/share/applications/cmake-gui.desktop
 sudo rm /usr/share/applications/lstopo.desktop
 sudo rm /usr/share/applications/java-java17-openjdk.desktop
@@ -72,16 +67,14 @@ sudo rm /usr/share/applications/linguist.desktop
 sudo rm /usr/share/applications/qdbusviewer.desktop
 
 sudo cp Alacritty.desktop /usr/share/applications/Alacritty.desktop
-sudo cp atril.desktop /usr/share/applications/atril.desktop
-sudo cp org.nomacs.ImageLounge.desktop /usr/share/applications/org.nomacs.ImageLounge.desktop
-sudo cp thunar.desktop /usr/share/applications/thunar.desktop
 sudo cp org.gnome.FileRoller.desktop /usr/share/applications/org.gnome.FileRoller.desktop
-sudo cp org.xfce.mousepad.desktop /usr/share/applications/org.xfce.mousepad.desktop
 sudo cp code.desktop /usr/share/applications/code.desktop
 sudo cp google-chrome.desktop /usr/share/applications/google-chrome.desktop
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface font-name 'Fira Sans Book 12'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'YourCursorThemeName'
 
 sudo mkdir /mnt/HOME
 echo '/dev/disk/by-uuid/4CC27C52C27C41EE /mnt/HOME auto nosuid,nodev,nofail,x-gvfs-show 0 0' | sudo tee -a /etc/fstab
