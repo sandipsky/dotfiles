d24c12e42d6e1251f67d8ea1c6caa8a1b73fccc2
sandipsky <sandipshakya75@gmail.com>
2025-05-27 20:41:27 +0545

    changes for 2025

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 USERNAME="sandip"
 
-sudo pacman -S hyprland hyprpaper xdg-desktop-portal-hyprland waybar hyprlock hypridle blueman rofi-wayland vlc ntfs-3g ffmpegthumbnailer alacritty xdg-user-dirs-gtk neofetch wget obs-studio qbittorrent starship dunst brightnessctl wlsunset grim slurp --noconfirm --needed
+sudo pacman -S hyprland hyprpaper xdg-desktop-portal-hyprland waybar hyprlock hypridle blueman rofi-wayland vlc ntfs-3g ffmpegthumbnailer alacritty xdg-user-dirs-gtk wget obs-studio qbittorrent starship dunst brightnessctl wlsunset grim slurp --noconfirm --needed
 
 #GNOME APPS
 sudo pacman -S nautilus gnome-music evince loupe gnome-calendar gnome-calculator file-roller gnome-text-editor gvfs-mtp gnome-themes-extra adwaita-icon-theme gnome-system-monitor gdm --noconfirm --needed
